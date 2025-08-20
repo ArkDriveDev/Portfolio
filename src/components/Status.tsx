11b02e5cbb8f9b0057e20b344acacf612a73082b
Arky <20231736@nbsc.edu.ph>
2025-08-20 21:02:12 +0800

    Added Status.css

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,29 +1,30 @@
 // Status.tsx
 import React from 'react';
 import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonGrid, IonRow, IonCol } from '@ionic/react';
+import '../CSS/Status.css';
 
 const Status: React.FC = () => {
   return (
-    <IonCard>
-      <IonCardHeader>
-        <IonCardTitle style={{ textAlign: 'center' }}>Status Open</IonCardTitle>
+    <IonCard className="status-card">
+      <IonCardHeader className="status-header">
+        <IonCardTitle className="status-title">Status Open</IonCardTitle>
       </IonCardHeader>
       
-      <IonCardContent>
+      <IonCardContent className="status-content">
         <IonGrid>
           <IonRow>
             {/* Left Side */}
             <IonCol>
-              <div>Appearance: A rank</div>
-              <div>Stamina: S rank</div>
-              <div>Energy: A rank</div>
+              <div className="stat-item">Appearance: <span className="rank">A rank</span></div>
+              <div className="stat-item">Stamina: <span className="rank">S rank</span></div>
+              <div className="stat-item">Energy Level: <span className="rank">A rank</span></div>
             </IonCol>
             
             {/* Right Side */}
             <IonCol>
-              <div>Coding: B rank</div>
-              <div>Diligence: A rank</div>
-              <div>Creativity: B rank</div>
+              <div className="stat-item">Programming skills: <span className="rank">B rank</span></div>
+              <div className="stat-item">Learning Skills: <span className="rank">A rank</span></div>
+              <div className="stat-item">Creativity: <span className="rank">B rank</span></div>
             </IonCol>
           </IonRow>
         </IonGrid>
