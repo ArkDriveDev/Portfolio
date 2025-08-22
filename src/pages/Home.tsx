62d1eb7616844d5bab4b4d444754c6494b682337
Arky <20231736@nbsc.edu.ph>
2025-08-22 20:07:52 +0800

    Date on watchface css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,9 @@ import Status from '../components/Status';
 import '../CSS/Home.css';
 
 const Home: React.FC = () => {
+  const dayText = "Friday August 22";
+  const yearText = "2025";
+
   return (
     <IonPage>
       <IonHeader>
@@ -20,25 +23,36 @@ const Home: React.FC = () => {
       >
         {/* Elegant Pocket Watch */}
         <div className="pocket-watch">
+          {/* Outer ring for date display */}
+          <div className="outer-ring">
+            {/* Top curved date (month and day) */}
+            <div className="date-top">
+              {dayText.split("").map((char, i) => (
+                <span key={i} style={{ transform: `rotate(${i * 10 - 60}deg)` }}>{char}</span>
+              ))}
+            </div>
+            
+            {/* Bottom year text (not curved) */}
+            <div className="year-bottom">
+              {yearText}
+            </div>
+          </div>
+          
+          {/* Inner circle for clock face */}
           <div className="watch-face">
+            {/* Clock numbers */}
             {[...Array(12)].map((_, i) => (
               <div key={i} className={`number number${i + 1}`}>{i + 1}</div>
             ))}
+
+            {/* Clock hands */}
             <div className="hand hour"></div>
             <div className="hand minute"></div>
             <div className="hand second"></div>
-
-            {/* Date display */}
-            <div className="date-top">
-              {"Friday August 22".split("").map((char, i) => <span key={i}>{char}</span>)}
-            </div>
-            <div className="date-bottom">
-              {"2025".split("").map((char, i) => <span key={i}>{char}</span>)}
-            </div>
           </div>
-
         </div>
 
+        {/* Avatar & Status */}
         <div className="avatar-status-container">
           <div className="avatar-wrapper">
             <Avatar />
@@ -48,12 +62,11 @@ const Home: React.FC = () => {
           </div>
         </div>
 
+        {/* Footer */}
         <div className="footer">
           <p>© 2023 Arky Roel U. Balaga. All Rights Reserved</p>
         </div>
       </IonContent>
-
-
     </IonPage>
   );
 };
